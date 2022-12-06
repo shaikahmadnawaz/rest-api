4801b995fd383821528123619025045059a9ace0
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2022-12-07 00:43:31 +0530

    routes

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ import express from "express";
 import { APP_PORT } from "./config";
 const app = express();
 
+import routes from "./routes";
+app.use("/api", routes);
+
 app.listen(APP_PORT, () => {
   console.log(`listening on port ${APP_PORT}`);
 });
